93162ed281b0008d36cc19aee1f53becf808e10c
agatamunii-dev <agata.munii@gmail.com>
2026-03-28 15:00:41 +0200

    Update Program.cs

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -1,3 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+using wwwww;
 
-Console.WriteLine("Hello, World!");
+Letter fff = new Letter("123", 55);
+Letter ggg = new Letter("222", 20);
+
+Parcel aa = new Parcel("9", 300, "5x33x15");
+Parcel bb = new Parcel("456787", 90, "10x6x44");
+
+fff.PrintInfo();
+bb.PrintInfo();
+
+CargoContainer <DeliveryItem> myFff = new CargoContainer<DeliveryItem>();
+myFff.AddItem(fff);
+myFff.AddItem(ggg);
+myFff.AddItem(aa);
+myFff.AddItem(bb);
+
+Console.WriteLine($"Сумарна ціна - {myFff.GetTotalCost()}");
