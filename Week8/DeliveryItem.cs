27c645322191344a782378bf96df2202cec04cbf
agatamunii-dev <agata.munii@gmail.com>
2026-03-28 14:08:46 +0200

    Add abstract DeliveryItem class for delivery tracking

---
+++ Week8/DeliveryItem.cs
@@ -0,0 +1,18 @@
+public abstract class DeliveryItem
+{
+    public string TrackingNumber { get; }
+    public double Weight { get; protected set; } 
+
+    public DeliveryItem(string name, double weight)
+    {
+        TrackingNumber = name;
+        Weight = weight;
+    }
+    
+    public abstract double CalculateCost();
+    
+    public virtual void PrintInfo()
+    {
+        Console.WriteLine($" Delivery : { TrackingNumber } | weight : { Weight }") ;
+    }
+}
