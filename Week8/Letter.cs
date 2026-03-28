a2267f9ebafcbf75ce2c83fcbed12cabefc1d60e
agatamunii-dev <agata.munii@gmail.com>
2026-03-28 14:59:27 +0200

    Update Letter.cs

--- Week8/Letter.cs
+++ Week8/Letter.cs
@@ -1,22 +1,26 @@
-public class Letter : DeliveryItem
+namespace wwwww
 {
-
-    public Letter(string TrackingNumber, double Weight) : base(TrackingNumber, Weight)
+    public class Letter : DeliveryItem
     {
-        
+
+        public Letter(string TrackingNumber, double Weight) : base(TrackingNumber, Weight)
+        {
+
+        }
+
+        public override double CalculateCost()
+        {
+            double Cost = 15 + Weight * 10;
+
+            return Cost;
+        }
+
+        public override void PrintInfo()
+        {
+            base.PrintInfo();
+            Console.WriteLine($" Ціна доставки - {15 + Weight * 10}");
+            Console.WriteLine();
+        }
     }
 
-    public override double CalculateCost()
-    {
-        double Cost = 15 + Weight * 10;
-        Console.WriteLine($"Ціна - {Cost}");
-
-        return Cost;
-    }
-
-    public override void PrintInfo()
-    {
-        base.PrintInfo();
-        Console.WriteLine($"Ціна доставки - {15 + Weight * 10}");
-    }
 }
